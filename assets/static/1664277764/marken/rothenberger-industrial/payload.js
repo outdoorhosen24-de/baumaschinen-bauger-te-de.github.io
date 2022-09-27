dffc7027b8b66fa54fd3d97f1d11f66a1ad88750
outdoorhosen24-de <outdoorhosen24-de@users.noreply.github.com>
2022-09-27 11:23:39 +0000

    deploy: e83fc67099ae210b07811db4db015548117b3de0

---
+++ assets/static/1664277764/marken/rothenberger-industrial/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/marken/rothenberger-industrial", {data:[{_img:{"/_ipx/f_webp,q_90,s_407x229/assets/images/images/rothenberger-styropor-und-hartschaumschneidegeraet-190-w-rothenberger-industrial-7261001-1.jpg":"\u002Fassets\u002Fimages\u002Frothenberger-styropor-und-hartschaumschneidegeraet-190-w-rothenberger-industrial-7261001-1.webp"}}],fetch:{},mutations:void 0});
